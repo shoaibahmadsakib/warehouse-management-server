03b13804a5d89c9c8eaef65415930bee0e5403ba
shoaibahmadsakib <shoaibahmadsakib@gmail.com>
2022-05-03 11:58:18 +0600

    add update and other item

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const ObjectId =require( 'mongodb').ObjectId
-require('dotenv').config()
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
 
 //middlewire
 app.use(cors());
@@ -13,34 +13,105 @@ app.use(express.json());
 
 //connect mongo db
 
-
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.jcioh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-  try{
+async function run() {
+  try {
     await client.connect();
     const userCollection = client.db("warehouse").collection("userinfo");
 
-
-    app.get('/userinfo',async (req,res)=>{
-        const query = {};
-        const cursor = userCollection.find(query)
-        const users = await cursor.toArray();
-        res.send(users);
-    })
+    app.get("/userinfo", async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
     app.post("/userinfo", async (req, res) => {
       const newService = req.body;
       const result = await userCollection.insertOne(newService);
       res.send(result);
     });
-  }
-  finally{
+    // get id
+    app.delete("/userinfo/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+    //for finding a single id
+    app.get("/userinfo/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
 
+    //updating a card
+    app.put("/userinfo/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateUser = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoct = {
+        $set: {
+          // name: updateUser.name,
+          // describtion: updateUser.describtion,
+          // price: updateUser.email,
+          quantity: updateUser.quantity,
+          // image: updateUser.image,
+        },
+      };
+      const result = await userCollection.updateOne(
+        filter,
+        updateDoct,
+        options
+      );
+      res.send(result);
+    });
+
+    //inventory
+    // app.put("/userinfo/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const updateUser = req.body;
+    //   const filter = { _id: ObjectId(id) };
+    //   const options = { upsert: true };
+    //   const updateDoct = {
+    //     $set: {
+          
+    //       quantity: updateUser.quantity,
+        
+    //     },
+    //   };
+    //   const result = await userCollection.updateOne(
+    //     filter,
+    //     updateDoct,
+    //     options
+    //   );
+    //   res.send(result);
+    // });
+    // app.get("/userinfo/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+    //   const result = await userCollection.findOne(query);
+    //   res.send(result);
+    // });
+
+
+
+    // app.post("increment/:id") ,async (req, res) => {
+    //   const { id } = req.params;
+    //   collection.updateOne({ _id: id }, { $inc: { views: 1 } });
+    //   return res.status(200).json({ msg: "OK" });
+    // };
+  } finally {
   }
 }
 run().catch(console.dir);
-
 
 //for making a base url for showing brawser
 app.get("/", (req, res) => {
@@ -49,8 +120,8 @@ app.get("/", (req, res) => {
 
 //for runnig the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
 //warehouse
 //yGGTnsPqCXmLAVTH
